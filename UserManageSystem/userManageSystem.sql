e8f0e874742cea3e3631b7385946e177c989fc74
“WhiteCloud7” <2334646904@qq.com>
2025-03-03 21:49:18 +0800

    上传用户管理系统的sql

---
+++ UserManageSystem/userManageSystem.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 8.0.13 : Database - usermanagesystem
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`usermanagesystem` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+
+USE `usermanagesystem`;
+
+/*Table structure for table `userinfo` */
+
+DROP TABLE IF EXISTS `userinfo`;
+
+CREATE TABLE `userinfo` (
+  `userId` bigint(255) NOT NULL AUTO_INCREMENT,
+  `username` varchar(10) NOT NULL,
+  `sex` varchar(2) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `userinfo` */
+
+insert  into `userinfo`(`userId`,`username`,`sex`,`phone`,`email`) values (1,'刘文杰','男','18671551575','2334646904@qq.com'),(2,'冯杰伟','女','13297679843','2334789054@139.com');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

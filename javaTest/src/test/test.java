69cab4469d2bb7d66bdc3260f29f707a98e12bbc
“WhiteCloud7” <2334646904@qq.com>
2025-03-28 23:57:20 +0800

    更新并发编程

--- javaTest/src/test/test.java
+++ javaTest/src/test/test.java
@@ -1,80 +1,83 @@
 package test;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
 
-public class test {
-    public static void main(String[] args) {
-        SharedResource resource = new SharedResource();
+import javax.xml.validation.Validator;
 
-        // 创建并启动多个线程
-        Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                resource.increment();
-            }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                synchronized (resource) {
-                    if (resource.value <= 3) {
-                        resource.notify(); 
-                    }
-                    resource.increment();
-                }
-            }
-        });
-
-        thread1.start();
-        thread2.start();
+public class test extends Thread {
+	a test = new a();
+    public static void main(String[] args){
+//        Thread t1 = new Thread(() -> {
+//            System.out.println("t1 执行完成");
+//        });
+//        Thread t2 = new Thread(() -> {
+//        	System.out.println("t2 执行完成");
+//        });
+//        t1.start();
+//        t2.start();
+    	t test = new t();
+    	for(int i=0;i<50;i++) {
+    		if(i%10==0&&i!=0) 
+    			test.writeData(i);
+    		else
+    			test.readData();
+    	}
     }
 }
 
-class SharedResource {
-    int value = 0;
-
-    public void increment() {
-        // 尝试获取锁
-        synchronized (this) {
-            try {
-                // 获取锁成功，访问和修改资源
-                value++;
-                if (value == 3) {
-                    this.wait(); // 当前线程在 resource 对象上等待
-                }
-                System.out.println(Thread.currentThread().getName() + " 增加后的值: " + value);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
-
-
-class mytheard extends Thread{
-	@Override
-	public void run() {
-		try {
-			sleep(10);
-			System.out.println("theard"+Thread.currentThread());
-		} catch (InterruptedException e) {
-			// TODO 自动生成的 catch 块
-			e.printStackTrace();
-		}
-		
+class a {
+	private int data = 3;
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+		System.out.println("我在写"+Thread.currentThread());
 	}
 }
 
-class mytheard2 extends Thread{
-	@Override
-	public void run() {
-		try {
-			wait();
-			System.out.println("theard"+Thread.currentThread());
-		} catch (InterruptedException e) {
-			// TODO 自动生成的 catch 块
-			e.printStackTrace();
-		}
-		
+class t {
+	ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+	ReadLock readlock = lock.readLock();
+	WriteLock writeLock = lock.writeLock();
+	a test = new a();
+	public void readData() {
+		new Thread(()->{
+			int d;
+			readlock.lock();
+			d=test.getData();
+			System.out.println("读取的数据为:"+d+"  "+Thread.currentThread());
+			readlock.unlock();
+		}).start();
+	}
+	
+	public void writeData(int data) {
+		new Thread(()->{
+			writeLock.lock();
+			test.setData(data);
+			writeLock.unlock();
+		}).start();
+	}
+	
+	public void readAndwrite() {
+		new Thread(()->{
+			int d;
+			readlock.lock();
+			d=test.getData();
+			System.out.println("读取的数据为:"+d+"  "+Thread.currentThread());
+			readlock.unlock();
+			writeLock.lock();
+			test.setData(d+1);
+			writeLock.unlock();
+		}).start();;
 	}
 }
+
